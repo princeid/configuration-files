d416a93701866397849cd366ca1b63e0403d0b2c
Mac <mac@Prince.local>
2020-08-05 00:01:30 +0100

    initial commit

---
+++ src/main/java/ConfigParser.java
@@ -0,0 +1,130 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class ConfigParser {
+    private String configFile;
+    private FileReader fileReader = null;
+    private BufferedReader bufferedReader = null;
+    private HashMap<String, String> map = new LinkedHashMap<>();
+
+    public String getConfigFile() {
+        return configFile;
+    }
+
+    public ConfigParser() {
+        this("src/main/config-files/config.txt");
+    }
+    public ConfigParser(String configFile) {
+        this.configFile = configFile;
+    }
+
+    /**
+     * @return
+     * @throws IOException
+     *
+     * This method (loadConfiguration) returns a Map containing the key/value pairs of the configuration file. Where there are multiple values for
+     * a key, only the first one is added to the map.
+     */
+    public HashMap<String, String> loadConfiguration() throws IOException {
+
+        try {
+            fileReader = new FileReader(configFile);
+            bufferedReader = new BufferedReader(fileReader);
+
+            String thisLine = bufferedReader.readLine();
+            String prefix = "";
+            String key = null;
+            String value;
+            int index;
+
+            /**
+             *  Loops until the end of the file.
+             */
+            while (thisLine != null) {
+                System.out.println(thisLine);
+
+                /**
+                 *  Checks if the current line being read is a sub heading or tag, for example "[application]" in order to extract the name and
+                 *  concatenate it to the following keys under it until another blank line or tag is met.
+                 */
+                if ((!thisLine.contains("=")) && (!thisLine.isBlank())) {
+                    prefix = thisLine.substring(1, thisLine.length()-1) + ".";  /* extracting the subheading/tag and concatenating with a dot "." */
+                    thisLine = bufferedReader.readLine(); /* Continues to read next line after meeting the condition for a subheading/tag and
+                    extracting the required portion */
+                    System.out.println(thisLine);
+
+                    /**
+                     *  The while loop below reads subsequent lines and processes their keys by concatenating the prefix with their respective keys
+                     *  until another subheading/tag or blank line is met.
+                     */
+                    try {
+                        while ((!thisLine.isBlank()) && (!thisLine.contains("["))) {
+                            if (thisLine.contains("=")) {
+                                index = thisLine.indexOf("=");
+                                key = prefix + thisLine.substring(0, index); /* Extracts the name portion from the current line being processed and
+                                concatenates the prefix to it to form the key */
+                                value = thisLine.substring(index+1); /* Extracts the value portion from the current line being processed to form
+                                the value */
+                                thisLine = bufferedReader.readLine();
+                                System.out.println(thisLine);
+                                if (!map.containsKey(key)) { /* Checks if the map does not contain the current key being processed, and if true it
+                                adds the key and value to the map */
+                                    map.put(key,value);
+                                }
+                            }
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+
+                /**
+                 *  Continues processing each line that is not part of a subheading/tag section and adding it to the map if it DOESN'T already
+                 *  exist in the map
+                 */
+                else if (thisLine.contains("=")) {
+                    prefix = "";
+                    index = thisLine.indexOf("=");
+                    key = prefix + thisLine.substring(0, index);
+                    value = thisLine.substring(index+1);
+
+                    if (!map.containsKey(key)) {
+                        map.put(key,value);
+                    }
+                }
+                thisLine = bufferedReader.readLine(); // Continues to next line
+            }
+            System.out.println(map);
+
+        } catch (Exception e) {
+            System.out.println("The filename '" + configFile + "' is not present on the file system! ");
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+                bufferedReader.close();
+            }
+        }
+
+        return map;
+    }
+
+    /**
+     * @param key The key in which the value to be returned is assigned
+     * @return Returns the value from the map
+     */
+    public String get(String key) {
+        return map.get(key);
+    }
+}
+
+/**
+ *   USING PROPERTIES CLASS, ASSUMING NO DUPLICATE KEY.
+ *
+ * if (inputStream != null) {
+ *      properties.load(inputStream);
+ *  } else {
+ *      throw new FileNotFoundException("Property file '" + configFile + "' not found in the classpath");
+ *    }
+ */
---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
+
+        /**
+         *  Set environment argument from the command line. "Goto -> Edit Configurations -> Program arguments"
+         *  The default environment is "production"
+         */
+
+        String relativePath = "src/main/config-files/";
+        String productionFile = "config.txt";
+        String stagingFile = "config-staging.txt";
+        String developmentFile = "config-development.txt";
+
+        String environment = args[0];
+        String configFile = relativePath + productionFile;
+
+        if (environment.equalsIgnoreCase("staging")) {
+            configFile = relativePath + stagingFile;
+        }
+        else if (environment.equalsIgnoreCase("development")) {
+            configFile = relativePath + developmentFile;
+        }
+
+        ConfigParser config = new ConfigParser();
+        System.out.println(config.loadConfiguration());
+        System.out.println(config.get("Loan"));
+    }
+}
